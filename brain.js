8766f86e225c2e790ee5f1a44098a58490c5c18d
Jahkem <jahshelby99@gmail.com>
2022-05-27 01:05:40 -0400

    rotating sidebar

--- brain.js
+++ brain.js
@@ -1,7 +1,2 @@
-const menu_toggle = document.querySelector('.menu-toggle');
-       const sidebar = document.querySelector('.sidebar');
 
-       menu_toggle.addEventListener('click', () => {
-           menu_toggle.classList.toggle('is-active');
-           sidebar.classList.toggle('is-active');
-       })
+
