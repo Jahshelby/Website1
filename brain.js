ad60d0ec330b83f5da004b38c9bd6b4d1feb1d8a
Jahkem <jahshelby99@gmail.com>
2022-06-02 00:22:36 -0400

    working on floating speech-bubble

--- brain.js
+++ brain.js
@@ -1,2 +1,10 @@
+const menu_toggle = document.querySelector('.menu-toggle');
+        const sidebar = document.querySelector('.sidebar');
 
+        menu_toggle.addEventListener('click', () => {
+        menu_toggle.classList.toggle('is-active');
+        sidebar.classList.toggle('is-active');
+        })
+        
+document.body.insertAdjacentHTML("afterbegin", "<p>Hello,<Br/>World!</p>");
 
